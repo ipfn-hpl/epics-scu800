0fedfe215e115e0b589250099fb9baf4319f1e9e
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-01 13:07:51 +0000

    updated PV/proto

--- iocBoot/iocscu800/st.cmd
+++ iocBoot/iocscu800/st.cmd
@@ -5,14 +5,32 @@
 
 < envPaths
 
+epicsEnvSet( "STREAM_PROTOCOL_PATH", "$(TOP)/db" )
+
 cd "${TOP}"
 
 ## Register all support components
 dbLoadDatabase "dbd/scu800.dbd"
 scu800_registerRecordDeviceDriver pdbbase
 
+## Load Serial drivers
+drvAsynSerialPortConfigure("RS485","/dev/rs485")
+asynSetOption("RS485", 0, "baud", "38400")
+asynSetOption("RS485", 0, "bits", "8")
+asynSetOption("RS485", 0, "parity", "none")
+asynSetOption("RS485", 0, "stop", "1")
+#asynSetOption("RS485", 0, "clocal", "Y")
+#asynSetOption("RS485", 0, "crtscts", "N")
+
+
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=bernardo")
+dbLoadRecords("db/edwards.db", "P=UUUUUU:,R=EDW:,A=1,BUS=RS485")
+
+var streamError 1
+var streamDebug 1
+#streamSetLogfile("stream_logfile.txt")
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
