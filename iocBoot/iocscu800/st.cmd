2bc4fee0d1f9319a28b4608041a3fbb21c960309
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-08 16:17:05 +0000

    Reading some PVs

--- iocBoot/iocscu800/st.cmd
+++ iocBoot/iocscu800/st.cmd
@@ -22,23 +22,23 @@ asynSetOption("RS485", 0, "stop", "1")
 #asynSetOption("RS485", 0, "clocal", "Y")
 #asynSetOption("RS485", 0, "crtscts", "N")
 
-drvAsynSerialPortConfigure("RS232","/dev/edwardsADC")
-asynSetOption("RS485", 0, "baud", "9600")
-asynSetOption("RS485", 0, "bits", "8")
-asynSetOption("RS485", 0, "parity", "none")
-asynSetOption("RS485", 0, "stop", "1")
-#asynSetOption("RS485", 0, "clocal", "Y")
+#drvAsynSerialPortConfigure("RS232","/dev/edwardsADC")
+#asynSetOption("RS485", 0, "baud", "9600")
+#asynSetOption("RS485", 0, "bits", "8")
+#asynSetOption("RS485", 0, "parity", "none")
+#asynSetOption("RS485", 0, "stop", "1")
+##asynSetOption("RS485", 0, "clocal", "Y")
 #asynSetOption("RS485", 0, "crtscts", "N")
-dbLoadRecords("db/edwards-adc.db", "P=UUU:,R=ADC:,A=1,BUS=RS232")
+#dbLoadRecords("db/edwards-adc.db", "P=UUU:,R=ADC:,A=1,BUS=RS232")
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=bernardo")
 # A must be asci code 49=0x31='1'
-dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,A=49,BUS=RS485")
-dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,A='50',BUS=RS485")
+dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,BUS=RS485")
+#dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,A='50',BUS=RS485")
 
 var streamError 1
-#var streamDebug 1
+var streamDebug 1
 #streamSetLogfile("stream_logfile.txt")
 
 
