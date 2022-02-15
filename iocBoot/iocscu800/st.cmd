fecfdcd9115e93b75fb13803cfdc5ad8b699c1ca
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-15 18:25:04 +0000

    Working for EDWARDS SCU800

--- iocBoot/iocscu800/st.cmd
+++ iocBoot/iocscu800/st.cmd
@@ -23,27 +23,16 @@ asynSetOption("RS485", 0, "stop", "1")
 #asynSetOption("RS485", 0, "clocal", "Y")
 #asynSetOption("RS485", 0, "crtscts", "N")
 
-#drvAsynSerialPortConfigure("RS232","/dev/edwardsADC")
-##drvAsynSerialPortConfigure("RS232","/dev/ttyUSB1")
-#asynSetOption("RS485", 0, "baud", "9600")
-#asynSetOption("RS485", 0, "bits", "8")
-#asynSetOption("RS485", 0, "parity", "none")
-#asynSetOption("RS485", 0, "stop", "1")
-##asynSetOption("RS485", 0, "clocal", "Y")
-##asynSetOption("RS485", 0, "crtscts", "N")
-
 ## Load record instances
 # How to user string parameters ? A must be asci code 49=0x31='1'
 #dbLoadRecords("db/edwards.db", "P=:,R=EDW:,BUS=RS485")
 #dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,A='50',BUS=RS485")
 
-#dbLoadRecords("db/edwards-adc.db", "P=Esther:,R=Vacuum:,A=1,BUS=RS232")
 dbLoadRecords("db/edwards.db", "P=Esther:,R=EDW:,BUS=RS485")
 
 var streamError 1
 # var streamDebug 1
 #streamSetLogfile("stream_logfile.txt")
-
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
