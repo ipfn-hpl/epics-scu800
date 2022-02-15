f784696720ca7fe8d18f982403f157be52b9b977
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-15 18:23:21 +0000

    Working for EDWARDS SCU800

--- iocBoot/iocscu800/st.cmd
+++ iocBoot/iocscu800/st.cmd
@@ -15,6 +15,7 @@ scu800_registerRecordDeviceDriver pdbbase
 
 ## Load Serial drivers
 drvAsynSerialPortConfigure("RS485","/dev/rs485")
+#drvAsynSerialPortConfigure("RS485","/dev/ttyUSB2")
 asynSetOption("RS485", 0, "baud", "38400")
 asynSetOption("RS485", 0, "bits", "8")
 asynSetOption("RS485", 0, "parity", "none")
@@ -23,19 +24,21 @@ asynSetOption("RS485", 0, "stop", "1")
 #asynSetOption("RS485", 0, "crtscts", "N")
 
 #drvAsynSerialPortConfigure("RS232","/dev/edwardsADC")
+##drvAsynSerialPortConfigure("RS232","/dev/ttyUSB1")
 #asynSetOption("RS485", 0, "baud", "9600")
 #asynSetOption("RS485", 0, "bits", "8")
 #asynSetOption("RS485", 0, "parity", "none")
 #asynSetOption("RS485", 0, "stop", "1")
 ##asynSetOption("RS485", 0, "clocal", "Y")
-#asynSetOption("RS485", 0, "crtscts", "N")
-#dbLoadRecords("db/edwards-adc.db", "P=UUU:,R=ADC:,A=1,BUS=RS232")
+##asynSetOption("RS485", 0, "crtscts", "N")
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=bernardo")
-# A must be asci code 49=0x31='1'
-dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,BUS=RS485")
+# How to user string parameters ? A must be asci code 49=0x31='1'
+#dbLoadRecords("db/edwards.db", "P=:,R=EDW:,BUS=RS485")
 #dbLoadRecords("db/edwards.db", "P=UUU:,R=EDW:,A='50',BUS=RS485")
+
+#dbLoadRecords("db/edwards-adc.db", "P=Esther:,R=Vacuum:,A=1,BUS=RS232")
+dbLoadRecords("db/edwards.db", "P=Esther:,R=EDW:,BUS=RS485")
 
 var streamError 1
 # var streamDebug 1
